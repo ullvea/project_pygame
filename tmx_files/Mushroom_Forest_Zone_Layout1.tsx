831149921d1ca5187a869f7fa2b137482a482939
ullvea <sofimastikova@gmail.com>
2025-01-01 20:32:22 +0300

    новая папка c tmx files

---
+++ tmx_files/Mushroom_Forest_Zone_Layout1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Mushroom_Forest_Zone_Layout1" tilewidth="32" tileheight="32" tilecount="2880" columns="96">
+ <image source="Mushroom_Forest_Zone_Layout.png" width="3072" height="960"/>
+</tileset>
