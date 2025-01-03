f20e40aa87011bd7ebd0dad31e595d83f5af78e6
ullvea <sofimastikova@gmail.com>
2025-01-03 18:29:32 +0300

    другая карта и спрайты на анимацию кирби НУЖНЫМ размером 16 на 16 тайлов

---
+++ tmx_files/Vegetable_Valley_1_Room_A_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Vegetable Valley 1 Room A" tilewidth="16" tileheight="16" tilecount="630" columns="63">
+ <image source="../data/NES - Kirbys Adventure - Vegetable Valley 1 Room A.png" width="1013" height="166"/>
+</tileset>
