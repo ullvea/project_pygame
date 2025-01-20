85b662d52185e78a460b130933d277656dd22ddb
ullvea <sofimastikova@gmail.com>
2025-01-20 22:53:08 +0300

    try to do a menu

--- menu.py
+++ menu.py
@@ -1,17 +1,21 @@
 from classes.base import *
 
 
-pygame.display.set_caption("Menu")
-back_ground = load_image("menu_white_and_purple.jpg")
-back_ground = pygame.transform.scale(back_ground, (700, 525))
-screen.fill((255, 255, 255))
-
-
 def main_menu():
-    while True:
+    pygame.init()
+    pygame.display.set_caption("Menu")
+    back_ground = load_image("menu_white_and_purple.jpg")
+    back_ground = pygame.transform.scale(back_ground, (700, 525))
+    screen.fill((255, 255, 255))
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
         screen.blit(back_ground, (0, 0))
 
         pygame.display.flip()
+    pygame.quit()
 
 
 main_menu()
