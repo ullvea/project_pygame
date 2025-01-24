7f7282140f8cf8669fb89ba0631689e6b4ff7ea4
User <id55i@yandex.ru>
2025-01-25 02:17:54 +0300

    buttons

--- menu.py
+++ menu.py
@@ -35,17 +35,31 @@ class Button:
 
 def main_menu():
     pygame.init()
-    # print(pygame.font.get_fonts())
+    # print(pygame.font.get_fonts()) системные шрифты
     pygame.display.set_caption("Menu")
     back_ground = load_image("pink_wallpaper.jpg")
+
     # изменяем размер картинки
     back_ground = pygame.transform.scale(back_ground, (700, 525))
     screen.fill((255, 255, 255))
     running = True
 
-    play_button = Button(50, 60, 100, 50, "PLAY")
+    # отрисовка кнопки звездочки
+    # play_button = Button(50, 60, 100, 50, "PLAY")
+    button_image1 = load_image('star.png')
+    button_image1 = pygame.transform.scale(button_image1, (150, 150))
+    button_rect1 = button_image1.get_rect(topleft=(180, 10))
+
+    button_image2 = load_image('star.png')
+    button_image2 = pygame.transform.scale(button_image2, (150, 150))
+    button_rect2 = button_image2.get_rect(topleft=(380, 10))
+
+    button_image3 = load_image('star.png')
+    button_image3 = pygame.transform.scale(button_image3, (50, 50))
+    button_rect3 = button_image3.get_rect(topleft=(10, 10))
 
     image = load_image("yellow_cursor2.png")
+
     title = load_image("kirbi.webp")
     title = pygame.transform.scale(title, (300, 200))
 
@@ -59,7 +73,10 @@ def main_menu():
             # отрисовка заднего фона
             screen.blit(back_ground, (0, 0))
             # отрисовка кнопки
-            play_button.draw(screen)
+            screen.blit(button_image1, button_rect1)
+            screen.blit(button_image2, button_rect2)
+            screen.blit(button_image3, button_rect3)
+            # play_button.draw(screen)
             # отрисовка названия
             screen.blit(title, (200, 150))
 
@@ -68,11 +85,14 @@ def main_menu():
                 # изображение курсора
                 screen.blit(image, (x, y))
 
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if button_rect1.collidepoint(event.pos):
+                    print("Кнопка нажата!")
 
             # play_button.draw(screen)
             pygame.mouse.set_visible(False)
 
-            play_button.event(event)
+            # play_button.event(event)
 
         pygame.display.flip()
     pygame.quit()
